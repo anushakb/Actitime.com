40a98fb74e57a23cab66b2b63968926e3aaa6e8e
Gubbi <Gubbi@Gubbi-PC>
2020-07-02 10:38:49 +0530

    my first commit

---
+++ actiTime/src/test/java/actiTime/Sample.java
@@ -0,0 +1,14 @@
+package actiTime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	
+@Test
+public void sample() {
+	Reporter.log("sample is working fine", true);
+}
+}
+
