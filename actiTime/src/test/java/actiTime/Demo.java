1e0ff9face3c7843d4b20d1b797f41fd77c7b8f5
Gubbi <Gubbi@Gubbi-PC>
2020-07-04 08:57:57 +0530

    new changes

---
+++ actiTime/src/test/java/actiTime/Demo.java
@@ -0,0 +1,12 @@
+package actiTime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void demo() {
+		Reporter.log("new changes", true);
+	}
+}
